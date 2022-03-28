ffbbfb108030472f2f7ad8841e930bd9a4ff6eb0
MarcosJRS <marcosjrs80@gmail.com>
2022-03-28 16:30:07 +0200

    useLayoutEffect para obtener las dimensiones de un <p> cuyo contenido es dinámico (frase de breaking bad)

--- src/components/blockQuote/BlockQuote.js
+++ src/components/blockQuote/BlockQuote.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useLayoutEffect, useRef } from 'react'
 
 export const BlockQuote = ({frase='', autor=''}) => {
+  //Guardamos una referencia
+  const blockQuoteDeBreakingBad = useRef();
+  //Lo mismo que useEffect, pero espera a terminar modificaciones del DOM
+  //Y así poder mostrar info sobre el
+  useLayoutEffect(() => {
+    console.log(blockQuoteDeBreakingBad.current.getBoundingClientRect());
+  }, [frase])
+
   return (
     <figure>
       <blockquote className="blockquote">
-        <p>{frase}</p>
+        <p ref={blockQuoteDeBreakingBad}>{frase}</p>
       </blockquote>
       <figcaption className="blockquote-footer">
         <cite title="Source Title">{autor}</cite>
